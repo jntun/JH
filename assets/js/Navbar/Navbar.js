a53d4c47d83edad8bf45fc28f32d4757534b0a32
Nastyyy <nerorazerx@gmail.com>
2017-05-23 21:51:22 -0700

    Minor navbar and gitignore changes
    - Removed webpack bundles from repo
    - Minor changes to the navbar

--- assets/js/Navbar/Navbar.js
+++ assets/js/Navbar/Navbar.js
@@ -3,24 +3,33 @@ import ReactDOM from 'react-dom'
 import { Navbar, NavDropdown, Nav, NavItem, MenuItem } from 'react-bootstrap'
 
 class MainNav extends Component {
+    unmount() {
+        ReactDOM.unmountComponentAtNode(document.getElementById('navbar'))
+    }
+
     render() {
         return (
             <div className='navbar'>
                 <Navbar collapseOnSelect>
                     <Navbar.Header>
                         <Navbar.Brand>
-                            <a href="">React</a>
+                            <a href="/heroscape">Heroscape</a>
                         </Navbar.Brand>
+                        <Navbar.Toggle />
                     </Navbar.Header>
                     <Navbar.Collapse>
-                    <Nav>
-                        <NavItem eventKey={1} href="">Cards</NavItem>
+                    <Nav className="nav-items">
+                        <MenuItem href="/heroscape/cards/" className="card-item">Cards</MenuItem>
                         <NavDropdown eventKey={2} title="Armys" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}>Army 1</MenuItem>
+                            <MenuItem eventKey={3.1} href="">Army 1</MenuItem>
                             <MenuItem eventKey={3.2}>Army 2</MenuItem>
                             <MenuItem eventKey={3.3}>Army 3</MenuItem>
                         </NavDropdown>
                     </Nav>
+                    <Nav pullRight>
+                        <MenuItem onClick={this.unmount}>Hide</MenuItem>
+                    </Nav>
+
                     </Navbar.Collapse>
                 </Navbar>
             </div>
